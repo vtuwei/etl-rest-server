63591ffd451a47b8048dbce88ff2213728275d0c
vtuwei <vtuwei@ampath.or.ke>
2016-08-12 10:53:07 +0300

    fixed etl file logger import

--- service/eid/eid-patient-cohort.service.js
+++ service/eid/eid-patient-cohort.service.js
@@ -4,7 +4,7 @@ var _ = require('underscore');
 var Promise=require('bluebird');
 var moment=require('moment');
 var config=require('../../conf/config');
-var etlLogger = require('../etl-file-logger');
+var etlLogger = require('../../etl-file-logger');
 module.exports = function(){
   function synchronizePatientCohort(patientUuIdCohort,reply){
     Promise.reduce(patientUuIdCohort,function(previous,patientUuId){
